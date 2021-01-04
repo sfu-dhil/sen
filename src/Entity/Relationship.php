57e3a1676358d0f15f516300b98864d71142eb35
Michael Joyce <ubermichael@gmail.com>
2021-01-04 13:44:26 -0800

    clean up the schema a tad.

--- src/Entity/Relationship.php
+++ src/Entity/Relationship.php
@@ -48,7 +48,7 @@ class Relationship extends AbstractEntity {
 
     /**
      * @var Person
-     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="relations")
      * @ORM\JoinColumn(nullable=false)
      */
     private $relation;
