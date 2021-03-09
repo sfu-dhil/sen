e779f59ed2b0e8efa956090e96523ce2f11575c8
Michael Joyce <mjoyce@sfu.ca>
2021-03-09 11:39:24 -0800

    fix migrations

---
+++ src/Util/SacramentColumnDefinitions.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) 2020 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Util;
+
+class SacramentColumnDefinitions {
+    public const first_name = 0;
+
+    public const last_name = 1;
+
+    public const title = 2;
+
+    public const birth_place = 3;
+
+    public const written_race = 4;
+
+    public const race_id = 5;
+
+    public const status = 6;
+
+    public const alias = 7;
+
+    public const native = 8;
+
+    public const occupation = 9;
+
+    public const sex = 10;
+
+    public const written_birth_date = 11;
+
+    public const birth_date = 12;
+
+    public const sac_status = 13;
+
+    public const event_baptism_place = 14;
+
+    public const event_written_baptism_date = 15;
+
+    public const event_baptism_date = 16;
+
+    public const event_baptism_source = 17;
+
+    public const godfather_first_name = 18;
+
+    public const godfather_last_name = 19;
+
+    public const godmother_first_name = 20;
+
+    public const godmother_last_name = 21;
+
+    //    public const event_baptism_date=22; duplicate column.
+    public const event_marriage_place = 23;
+
+    public const event_written_marriage_date = 24;
+
+    public const event_marriage_date = 25;
+
+    public const event_marriage_source = 26;
+
+    public const event_marriage_witness1_last_name = 27;
+
+    public const event_marriage_witness1_first_name = 28;
+
+    public const event_marriage_witness2_last_name = 29;
+
+    public const event_marriage_witness2_first_name = 30;
+
+    public const event_written_death_date = 31;
+
+    public const event_death_date = 32;
+
+    public const event_death_place = 33;
+
+    public const event_death_source = 34;
+
+    public const misc_notes = 35;
+
+    public const father_first_name = 36;
+
+    public const father_last_name = 37;
+
+    public const mother_first_name = 38;
+
+    public const mother_last_name = 39;
+}
