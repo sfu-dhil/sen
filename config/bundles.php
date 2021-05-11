dffc30580a97e8de26dffa744e0c62702bb290bf
Michael Joyce <mjoyce@sfu.ca>
2021-05-11 11:26:20 -0700

    maker bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -21,6 +21,7 @@ return [
     Nines\DublinCoreBundle\NinesDublinCoreBundle::class => ['all' => true],
     Nines\EditorBundle\NinesEditorBundle::class => ['all' => true],
     Nines\FeedbackBundle\NinesFeedbackBundle::class => ['all' => true],
+    Nines\MakerBundle\NinesMakerBundle::class => ['dev' => true],
     Nines\UserBundle\NinesUserBundle::class => ['all' => true],
     Nines\UtilBundle\NinesUtilBundle::class => ['all' => true],
 ];
