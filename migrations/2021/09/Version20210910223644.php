b071c729302b6ec85aec7b4cb5ebacd8f36feba7
Michael Joyce <mjoyce@sfu.ca>
2021-09-29 11:47:29 -0700

    ws

--- migrations/2021/09/Version20210910223644.php
+++ migrations/2021/09/Version20210910223644.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * (c) 2021 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -10,20 +16,16 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210910223644 extends AbstractMigration
-{
-    public function getDescription() : string
-    {
+final class Version20210910223644 extends AbstractMigration {
+    public function getDescription() : string {
         return '';
     }
 
-    public function up(Schema $schema) : void
-    {
+    public function up(Schema $schema) : void {
         $this->addSql('ALTER TABLE link CHANGE url url VARCHAR(500) NOT NULL, CHANGE text text VARCHAR(200) DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
-    {
+    public function down(Schema $schema) : void {
         $this->throwIrreversibleMigrationException();
     }
 }
